59aa6a0e46f6ffc6361c19811387e7ae0ec48223
Shafa1210 <111202415883@mhs.dinus.ac.id>
2026-04-03 19:52:07 +0700

    Add files via upload

---
+++ app/Views/v_keranjang.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Halaman Keranjang</title>
+</head>
+<body>
+    <h1>ini halaman keranjang</h1>
+    <a href="<?= site_url('/') ?>">Kembali ke Home</a>
+</body>
+</html>
---
+++ app/Views/v_produk.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Halaman Produk</title>
+</head>
+<body>
+    <h1>ini halaman Produk</h1>
+    <a href="<?= site_url('/') ?>">Kembali ke Home</a>
+</body>
+</html>
